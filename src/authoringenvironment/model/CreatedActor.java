2c0dcd1e5c0093b347ec48549325a9d5debf151d
azhao96 <azhao96@gmail.com>
2016-03-26 16:42:02 -0400

    wrote API for createdActor

--- src/authoringenvironment/model/CreatedActor.java
+++ src/authoringenvironment/model/CreatedActor.java
@@ -6,5 +6,41 @@ package authoringenvironment.model;
 
 public interface CreatedActor {
 
+	/**
+	 * Sets the image of the actor by accessing the image through the ImageLibrary.
+	 * @param filename: name of image file.
+	 */
 	public void setImage(String filename);
+	
+	/**
+	 * Sets the name of the actor.
+	 * @param name: name of actor.
+	 */
+	public void setName(String name);
+	
+	/**
+	 * Sets whether or not the actor responds to a trigger (mouse click, key press, or collision).
+	 * @param isReactive: true if the actor responds to a trigger event, false o.w.
+	 */
+	public void setReactive(boolean isReactive);
+	
+	/**
+	 * Sets the position of the actor.
+	 * @param x: x-position.
+	 * @param y: y-position.
+	 */
+	public void setPosition(double x, double y);
+	
+	/**
+	 * Adds a rule to the actor.
+	 * @param trigger: event that triggers the action (e.g. MouseClick, KeyPress, Collision).
+	 * @param action: action that the actor carries out in response (e.g. MoveUp, MoveDown, Die).
+	 */
+	public void addRule(String trigger, String action);
+	
+	/**
+	 * Returns the info necessary to create the actor in the game engine in a specified order and format.
+	 * @return: actor's info in a specified order and format.
+	 */
+	public String getInfo();
 }
