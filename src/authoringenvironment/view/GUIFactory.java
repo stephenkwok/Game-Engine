1a4138aa05b10ca91094989dec0694b26ca88199
ANNIE <anniekatelyntang@gmail.com>
2016-03-26 18:11:34 -0400

    authoring environment view interfaces

--- src/authoringenvironment/view/GUIFactory.java
+++ src/authoringenvironment/view/GUIFactory.java
@@ -1,5 +1,17 @@
 package authoringenvironment.view;
 
-public interface GUIFactory {
+/**
+ * Instantiates IGUIElements based on a ResourceBundle String key passed into createNewGUIObject(String nodeTypeKey).
+ *
+ * @author AnnieTang
+ */
 
+public interface GUIFactory {
+	
+	/**
+	 * Creates new IGUIElement based on nodeTypeKey passed in. 
+	 * @param nodeTypeKey
+	 * @return IGUIElement
+	 */
+	IGUIElement createNewGUIObject(String nodeTypeKey);
 }
--- src/authoringenvironment/view/IGUIElement.java
+++ src/authoringenvironment/view/IGUIElement.java
@@ -1,5 +1,22 @@
 package authoringenvironment.view;
 
-public interface IGUIElement {
+import javafx.scene.Node;
 
+/**
+ * This interface is part of the internal API. To add new features, you
+ * can create an instance of an Element class that implements this parent interface. 
+ * @author AnnieTang
+ */
+public interface IGUIElement {
+	/**
+     * This method creates the GUI element and passes it to the GUI class as a Node
+     * type.
+     * @return 
+     */
+    Node createNode();
+
+    /**
+     * This method updates the Node based on new information.
+     */
+    void updateNode();
 }
