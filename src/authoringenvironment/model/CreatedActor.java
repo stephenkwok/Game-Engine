f323e6400da95ecb51d9c17af6f781172c990ca5
azhao96 <azhao96@gmail.com>
2016-03-26 16:30:01 -0400

    created ImageLibrary interface

--- src/authoringenvironment/model/CreatedActor.java
+++ src/authoringenvironment/model/CreatedActor.java
@@ -1,5 +1,10 @@
 package authoringenvironment.model;
 
+// This class represents the actors that the user will create. It holds the relevant data that must be saved in the XML
+// and will implement the IActor interface, and it will also extend the GestureSource class so that the user can interact
+// with it from the GUI
+
 public interface CreatedActor {
 
+	public void setImage(String filename);
 }
