ccea6e72fb555c43c12b5433101fd7b5962b5f9d
michellexchen <michellechen617@gmail.com>
2016-03-31 17:41:03 -0400

    moveleft action added

---
+++ src/usecases/MoveLeft.java
@@ -0,0 +1,32 @@
+package usecases;
+
+import java.util.List;
+
+/**
+ * Created by Michelle on 3/31/16.
+ */
+
+public class MoveLeft extends Action {
+
+    private static final int LEFT_ANGLE = 90;
+    private double distance;
+
+    /**
+     * Takes in reference to the Actor it will change along with the argument it will require to do so
+     *
+     * @param assignedActor The Actor that will be changed
+     * @param args          The arguments required to perform the change
+     */
+    public MoveLeft(Actor assignedActor, List<Object> args) {
+        super(assignedActor);
+        distance = (Double) args.get(0);
+    }
+
+    /**
+     * Moves the Actor to the left by the distance provided in the arguments
+     */
+    @Override
+    public void perform() {
+        getActor().move(distance, LEFT_ANGLE);
+    }
+}
