f211e88ca4eb46d86185c50ccadc85958c33a4e1
ANNIE <anniekatelyntang@gmail.com>
2016-04-02 17:50:15 -0400

    added sound library, has functionality to preview sounds yay

--- src/authoringenvironment/view/TabImages.java
+++ src/authoringenvironment/view/TabImages.java
@@ -1,7 +1,4 @@
 package authoringenvironment.view;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,31 +8,17 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class TabImages extends TabParent {
-	private static final String IMAGE_RESOURCE = "actorimages";
 	private static final int STANDARD_IMAGE_HEIGHT = 20;
 	private ObservableList<Label> imageLabels; 
-	private List<String> imageNames;
-	
 	public TabImages(ResourceBundle myResources, String tabText) {
 		super(myResources,tabText);
-		fillImageNames();
-	}
-	
-	/**
-	 * Fills list of actor images ased on which images are in /actorimages file directory.
-	 */
-	private void fillImageNames(){
-		imageNames = new ArrayList<>();
-		File imageDir = new File(IMAGE_RESOURCE);
-		for(File imageFile: imageDir.listFiles()){
-			imageNames.add(imageFile.getName());
-		}
 	}
 
 	@Override
 	void setContent() {
+		fillFileNames();
 		imageLabels = FXCollections.observableArrayList();
-		for(String imageName: imageNames){
+		for(String imageName: fileNames){
 			ImageView iv = new ImageView(new Image(getClass().getClassLoader().getResourceAsStream(imageName)));
 			iv.setFitHeight(STANDARD_IMAGE_HEIGHT);
 			iv.setPreserveRatio(true);
--- src/authoringenvironment/view/TabParent.java
+++ src/authoringenvironment/view/TabParent.java
@@ -1,26 +1,39 @@
 package authoringenvironment.view;
-
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import javafx.scene.control.Tab;
-
 abstract class TabParent{
 	protected Tab tab;
 	private String tabText;
 	protected ResourceBundle myResources;
+	protected List<String> fileNames;
 	
 	public TabParent(ResourceBundle myResources, String tabText) {
 		this.tabText = tabText;
 		this.myResources = myResources;
 	}
-
+	
+	/**
+	 * Fills list of Strings based on which images/sounds are in the given file directory.
+	 */
+	protected void fillFileNames(){
+		fileNames = new ArrayList<>();
+		File imageDir = new File(myResources.getString(tabText));
+		for(File imageFile: imageDir.listFiles()){
+			fileNames.add(imageFile.getName());
+		}
+	}
+	
 	public Tab createTab() {
 		tab = new Tab(tabText);
 		setContent();
 		return tab;
 	}
-
-	private void updateNode(){
+	
+	public void updateNode(){
 		setContent();
 	}
 	
