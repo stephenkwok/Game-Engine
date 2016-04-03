14b4020a4cbf173bae46093285a1cc2538b9e9ea
azhao96 <azhao96@gmail.com>
2016-04-02 23:28:06 -0400

    tried to set up left pane of level editing environment - more stuff

---
+++ src/authoringenvironment/view/TabLevelAttributes.java
@@ -0,0 +1,21 @@
+package authoringenvironment.view;
+
+import java.util.ResourceBundle;
+
+/**
+ * Tab for setting level attributes to go in the Inspector Pane in the Level Editing Environment GUI.
+ * @author amyzhao
+ *
+ */
+public class TabLevelAttributes extends TabParent {
+
+	public TabLevelAttributes(ResourceBundle myResources, String tabText) {
+		super(myResources, tabText);
+	}
+
+	@Override
+	void setContent() {
+		
+	}
+
+}
