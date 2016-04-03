2ecfe3631487cb1325fb6cba7da77cafb0e1632f
Stephen Kwok <smk44@duke.edu>
2016-04-03 16:20:27 -0400

    stashing changes

--- src/authoringenvironment/view/GUIMainScreen.java
+++ src/authoringenvironment/view/GUIMainScreen.java
@@ -1,14 +1,16 @@
 package authoringenvironment.view;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import authoringenvironment.controller.Controller;
 import authoringenvironment.model.IEditableGameElement;
 import gameengine.controller.ILevel;
 import gameengine.model.IActor;
-import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
@@ -16,6 +18,7 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import usecases.Actor;
@@ -30,41 +33,57 @@ public class GUIMainScreen implements IGUI {
 	private ScrollPane levelScrollPane;
 	private HBox scrollPaneContainer;
 	private BorderPane borderPane;
-	private List<ClickableLabel> levelLabels;
-	private List<ClickableLabel> actorLabels;
+	private List<ClickableLabel> clickableLabels;
 
 	public GUIMainScreen(Controller controller) {
 		this.controller = controller;
+		clickableLabels = new ArrayList<ClickableLabel>();
 		initializeEnvironment();
 	}
 
 	public void initializeEnvironment() {
-		Stage stage = controller.getStage();
-		borderPane = new BorderPane();
-		borderPane.setStyle("-fx-border-color: red;");
-		borderPane.prefHeightProperty().bind(stage.heightProperty());
-		borderPane.prefWidthProperty().bind(stage.widthProperty());
-		scrollPaneContainer = new HBox();
-		scrollPaneContainer.setStyle("-fx-border-color: blue;");
-		HBox.setHgrow(scrollPaneContainer, Priority.ALWAYS);
-		scrollPaneContainer.prefHeightProperty().bind(borderPane.heightProperty());
-		scrollPaneContainer.prefWidthProperty().bind(borderPane.widthProperty());
+		initBorderPane();	
+		initScrollPaneContainer();
 		initLabelContainers();
-		actorScrollPane = initScrollPane(actorLabelContainer.widthProperty(), actorLabelContainer);
-		levelScrollPane = initScrollPane(levelLabelContainer.widthProperty(), levelLabelContainer);
+		actorScrollPane = initScrollPane(actorLabelContainer);
+		levelScrollPane = initScrollPane(levelLabelContainer);
 		scrollPaneContainer.getChildren().addAll(levelScrollPane, actorScrollPane);
 		borderPane.setCenter(scrollPaneContainer);
 	}
 	
+	private void initBorderPane() {
+		Stage stage = controller.getStage();
+		borderPane = new BorderPane();
+//		borderPane.setStyle("-fx-border-color: red;");
+		borderPane.prefHeightProperty().bind(stage.heightProperty());
+		borderPane.prefWidthProperty().bind(stage.widthProperty());
+	}
+	
+	private void initScrollPaneContainer() {
+		scrollPaneContainer = new HBox();
+//		scrollPaneContainer.setStyle("-fx-border-color: blue;");
+//		HBox.setHgrow(scrollPaneContainer, Priority.ALWAYS);
+		bindNodeSizeToParentSize(scrollPaneContainer, borderPane);
+	}
+	
 	private void initLabelContainers() {
-		actorLabelContainer = new VBox();
-		levelLabelContainer = new VBox();
-		actorLabelContainer.prefWidthProperty().bind(scrollPaneContainer.widthProperty());
-		actorLabelContainer.prefHeightProperty().bind(scrollPaneContainer.heightProperty());
-		levelLabelContainer.prefWidthProperty().bind(scrollPaneContainer.widthProperty());
-		levelLabelContainer.prefHeightProperty().bind(scrollPaneContainer.heightProperty());
-		actorLabelContainer.getChildren().add(createButton("Add New Actor", e -> addActor()));
-		levelLabelContainer.getChildren().add(createButton("Add New Level", e -> addLevel()));
+		actorLabelContainer = createLabelContainer("Add New Actor", e -> addActor());
+		levelLabelContainer = createLabelContainer("Add New Level", e -> addLevel());
+	}
+	
+	private VBox createLabelContainer(String buttonText, EventHandler<ActionEvent> buttonEvent) {
+		VBox container = new VBox();
+		bindNodeSizeToParentSize(container, scrollPaneContainer);
+		// HARD-CODED VALUE
+		container.setMaxWidth(630);
+		container.setAlignment(Pos.TOP_CENTER);
+		container.getChildren().add(createButton(buttonText, buttonEvent));
+		return container;
+	}
+	
+	private void bindNodeSizeToParentSize(Region child, Region parent) {
+		child.prefWidthProperty().bind(parent.widthProperty());
+		child.prefHeightProperty().bind(parent.heightProperty());
 	}
 	
 	private Button createButton(String buttonText, EventHandler<ActionEvent> handler) {
@@ -73,38 +92,42 @@ public class GUIMainScreen implements IGUI {
 		return button;
 	}
 	
-	private ScrollPane initScrollPane(ReadOnlyDoubleProperty bindingProperty, VBox container) {
+	private ScrollPane initScrollPane(VBox labelContainer) {
 		ScrollPane scrollPane = new ScrollPane();
-		scrollPane.setContent(container);
+		scrollPane.setContent(labelContainer);
 		return scrollPane;
 	}
 
 	public Pane getPane() {
 		return borderPane;
 	}
-
-	private void updateLabels(List<ClickableLabel> labels) {
-		labels.stream().forEach(label -> label.update());
-	}
 	
 	private void addActor() {
 		IEditableGameElement newActor = new Actor();
 		ActorLabel label = new ActorLabel(newActor, controller);
-		actorLabelContainer.getChildren().add(label);
+		processLabel(actorLabelContainer, label);
 		controller.goToActorEditing((IActor) newActor);
 	}
 	
 	private void addLevel() {
 		IEditableGameElement newLevel = new Level();
 		LevelLabel label = new LevelLabel(newLevel, controller);
-		levelLabelContainer.getChildren().add(label);
+		processLabel(levelLabelContainer, label);
 		controller.goToLevelEditing((ILevel) newLevel);
+	}
+	
+	private void processLabel(VBox container, ClickableLabel label) {
+		container.getChildren().add(label);
+		clickableLabels.add(label);
 	}
 	
 	@Override
 	public void updateAllNodes() {
-		updateLabels(levelLabels);
-		updateLabels(actorLabels);
+		updateLabels();
+	}
+	
+	private void updateLabels() {
+		clickableLabels.stream().forEach(label -> label.update());
 	}
 
 	@Override
