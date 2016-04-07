f2a35b3af932edcd21eb1361340760ec89ea3a9c
bobbywang000 <bcw22@duke.edu>
2016-04-07 17:49:20 -0400

    Preliminary HUD Screen

--- src/gameplayer/view/HUDScreen.java
+++ src/gameplayer/view/HUDScreen.java
@@ -1,21 +1,51 @@
 package gameplayer.view;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
 import javafx.collections.FXCollections;
+import javafx.collections.MapChangeListener;
+import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
+import javafx.collections.MapChangeListener.Change;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Text;
 
 public class HUDScreen extends Window implements IHUDScreen{
 	
-	private ObservableMap<String, String> status = FXCollections.observableHashMap();
+	private ObservableMap<String, Object> status = FXCollections.observableHashMap();
+	Map<String, Integer> valueToRowMap;
+	Map<Integer, String> rowToValueMap;
 	
-	public HUDScreen(double width, double height) {
+	public HUDScreen(double width, double height, ObservableMap<String, Object> status, Map<Integer, String> rowToValueMap) {
 		super(width, height);
+		this.status = status;
+		this.rowToValueMap = rowToValueMap;
+		for (int i = 0; i<rowToValueMap.size(); i++) {
+			valueToRowMap.put(rowToValueMap.get(i), i);
+		}
 	}
-
+	
+	
+	public HUDScreen(double width, double height, ObservableMap<String, Object> status) {
+		this(width, height, status, new HashMap<Integer, String>());
+		int i = 0;
+		for (String value : status.keySet()) {
+			rowToValueMap.put(i++, value);
+		}
+	}
+	
+	
+	
+	
 	@Override
 	public void update() {
 		
@@ -25,9 +55,31 @@ public class HUDScreen extends Window implements IHUDScreen{
 	public Scene init() {
 		Scene myScene = new Scene(super.getRoot(), super.getWidth(), super.getHeight());
 		
-		Text level = new Text(status.get("Level"));
+        ObservableList<String> keys = FXCollections.observableArrayList();
+        ObservableList<String> values = FXCollections.observableArrayList();
+        ListView<String> keyView = new ListView<>(keys);
+        ListView<String> valueView = new ListView<>(values);
+        keyView.setMaxWidth(myScene.getWidth()/2);
+        valueView.setMaxWidth(myScene.getWidth()/2);
+        
+        BorderPane container = new BorderPane();
+        container.setLeft(keyView);
+        container.setRight(valueView);
 		
-		super.getRoot().getChildren().add(level);
+        for (int i = 0; i<status.size(); i++) {
+        	keys.add(rowToValueMap.get(i));
+        	values.add(status.get(rowToValueMap.get(i)).toString());
+        }
+        
+        status.addListener(new MapChangeListener<String, Object>() {
+			@Override
+			public void onChanged(Change<? extends String, ? extends Object> change) {
+				int rownum = valueToRowMap.get(change.getKey());
+				values.set(rownum, change.getValueAdded().toString());
+			}
+        });
+		
+		super.getRoot().getChildren().add(container);
 		
 		return myScene;
 	}
