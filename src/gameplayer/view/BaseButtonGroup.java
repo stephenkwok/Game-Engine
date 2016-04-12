f8bd0259cc771de90a9dc271b217544058b51ce9
MICHAEL FIGUEIRAS <mdf15@duke.edu>
2016-04-11 22:07:46 -0400

    added restart button

---
+++ src/gameplayer/view/BaseButtonGroup.java
@@ -0,0 +1,9 @@
+package gameplayer.view;
+
+public class BaseButtonGroup {
+
+	public BaseButtonGroup() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/gameplayer/view/GameButtonGroup.java
@@ -0,0 +1,9 @@
+package gameplayer.view;
+
+public class GameButtonGroup {
+
+	public GameButtonGroup() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
