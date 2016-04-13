845ac7ff74da88034d40ba627ae00a9d6d14c838
azhao96 <azhao96@gmail.com>
2016-04-12 20:13:19 -0400

    commented methods for buttons and comboboxes

--- src/gui/view/ButtonNewActor.java
+++ src/gui/view/ButtonNewActor.java
@@ -4,16 +4,25 @@ import authoringenvironment.controller.Controller;
 import gui.controller.IScreenController;
 
 /**
- * 
+ * Button for adding a new actor.
  * @author amyzhao
  *
  */
 public class ButtonNewActor extends ButtonParent {
 
+	/**
+	 * Constructs a button for adding a new actor.
+	 * @param myController: environment controller.
+	 * @param buttonText: text for the button.
+	 * @param imageName: image for the button.
+	 */
 	public ButtonNewActor(IScreenController myController, String buttonText, String imageName) {
 		super(myController, buttonText, imageName);
 	}
 
+	/**
+	 * On click, add a new actor via the controller.
+	 */
 	@Override
 	protected void setButtonAction() {
 		button.setOnAction(e -> ((Controller) myController).addActor());
