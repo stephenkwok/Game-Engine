1a3866f2e7cfb027dfb942a54b9405f66ed864d1
Stephen Kwok <smk44@duke.edu>
2016-04-12 22:45:57 -0400

    Implemented setting of HUDElements map in GameInfo and GUIGameEditingEnvironment

--- src/gui/view/TextFieldGameNameEditor.java
+++ src/gui/view/TextFieldGameNameEditor.java
@@ -14,6 +14,7 @@ public class TextFieldGameNameEditor extends TextFieldWithButton{
 	public TextFieldGameNameEditor(String labelText, String prompt, Double textFieldWidth) {
 		super(labelText, prompt, textFieldWidth);
 		setButtonAction(e -> ((GameInfo) getEditableElement()).setMyName(getTextFieldInput()));
+		setTextFieldHGrow();
 	}
 
 	@Override
