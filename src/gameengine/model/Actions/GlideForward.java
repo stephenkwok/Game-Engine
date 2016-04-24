38a40d1cccec98932fca54d1be93e37258e9fa5e
Justin Bergkamp <j.bergkamp2@gmail.com>
2016-04-24 14:39:41 -0400

    Fixed bug in MoveForward/MoveBackward

--- src/gameengine/model/Actions/GlideForward.java
+++ src/gameengine/model/Actions/GlideForward.java
@@ -1,11 +1,12 @@
 package gameengine.model.Actions;
 
 import gameengine.model.Actor;
+import gameengine.model.IPlayActor;
 
 public class GlideForward extends GlidingAction {
 
-	public GlideForward(Actor assignedActor) {
-		super(assignedActor);
+	public GlideForward(IPlayActor actor1) {
+		super(actor1);
 	}
 
 	@Override
