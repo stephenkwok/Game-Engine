f488dae39726f81e279a316a4c1a3dfab09436e6
torrescar <cmt57@duke.edu>
2016-04-25 03:50:25 -0400

    debugs #359

--- src/gameplayer/controller/BaseScreenController.java
+++ src/gameplayer/controller/BaseScreenController.java
@@ -112,7 +112,8 @@ public class BaseScreenController extends BranchScreenController {
 				Class<?> myClass = Class.forName(myResources.getString(methodName));
 				Object arg2 = myClass.cast(myList.get(1));
 				Class[] parameterTypes = { myClass };
-				this.getClass().getDeclaredMethod(methodName, parameterTypes).invoke(this, arg2);
+				Object[] parameters = { arg2 };
+				this.getClass().getDeclaredMethod(methodName, parameterTypes).invoke(this, parameters);
 			}
 		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException
 				| SecurityException | ClassNotFoundException e) {
