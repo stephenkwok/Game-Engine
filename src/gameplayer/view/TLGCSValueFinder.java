47402fbc73984da8c9ce8970b91eb3d7d0b566ce
bobbywang000 <bcw22@duke.edu>
2016-04-28 16:29:22 -0400

    Changed valueFinder and added sound player

--- src/gameplayer/view/TLGCSValueFinder.java
+++ src/gameplayer/view/TLGCSValueFinder.java
@@ -6,12 +6,11 @@ import voogasalad.util.hud.source.*;
 
 public class TLGCSValueFinder implements IValueFinder {
 
-	private HUDController controller;
 	private Game data; //for other projects, your data will be a different class
 	
 	@Override
-	public Property find(String key) {
-		Property ret = null;
+	public Property<?> find(String key) {
+		Property<?> ret = null;
 		switch (key.toLowerCase()) {
 			case "points":
 				ret = data.getCurrentLevel().getMainCharacter().getAttribute(AttributeType.POINTS).getProperty();
@@ -23,17 +22,12 @@ public class TLGCSValueFinder implements IValueFinder {
 				//
 				break;
 			default:
-				ret = new Property("Value Not Found", key);
+				ret = new Property<String>("Value Not Found", key);
 				break;
 		}
-		ret.addObserver(controller);
 		return ret;
 	}
 
-	@Override
-	public void setController(HUDController controller) {
-		this.controller = controller;
-	}
 
 	@Override
 	public void setDataSource(Object dataSource) throws IllegalArgumentException {
