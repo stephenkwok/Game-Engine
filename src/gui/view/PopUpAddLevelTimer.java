f8681c4e3a6bb1607f3a4ed5b03d2f9dbdbc5540
azhao96 <azhao96@gmail.com>
2016-04-28 13:02:19 -0400

    added actions combobox to leveltimer

--- src/gui/view/PopUpAddLevelTimer.java
+++ src/gui/view/PopUpAddLevelTimer.java
@@ -1,14 +1,25 @@
 package gui.view;
 
+import java.util.Arrays;
+import java.util.Observable;
+import java.util.Observer;
 import java.util.ResourceBundle;
 
+import authoringenvironment.model.IActionCreator;
+import authoringenvironment.view.AttributeTriggerAndActionCreator;
+import authoringenvironment.view.CreateActorActionCreator;
+import authoringenvironment.view.LoseGameActionCreator;
+import authoringenvironment.view.NextLevelActionCreator;
+import authoringenvironment.view.WinGameActionCreator;
+import gameengine.controller.Level;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class PopUpAddLevelTimer extends PopUpParent {
+public class PopUpAddLevelTimer extends PopUpParent implements Observer {
 	private static final String INITIAL = "initialTime";
 	private static final String TRIGGER = "triggerTime";
 	private static final String ACTION = "action";
@@ -17,16 +28,23 @@ public class PopUpAddLevelTimer extends PopUpParent {
 	private static final String RESOURCES = "levelTimer";
 	private static final String MINUTES = "Minutes:";
 	private static final String SECONDS = "Seconds:";
+	private static final String ACTION_PROMPT = "Select";
+	private static final String DELIMITER = ",";
+	private static final String DONE = "Done";
 	private ResourceBundle myResources;
 	private TextField myInitialMinutes;
 	private TextField myInitialSeconds;
 	private TextField myTriggerMinutes;
 	private TextField myTriggerSeconds;
-	private ComboBox myAction;
+	private ComboBoxLevelTriggerAndAction myAction;
+	private VBox myActionCreator;
+	private Level myLevel;
+	private Button myButton;
 	
-	public PopUpAddLevelTimer(int popUpWidth, int popUpHeight) {
+	public PopUpAddLevelTimer(int popUpWidth, int popUpHeight, Level level) {
 		super(popUpWidth, popUpHeight);
 		myResources = ResourceBundle.getBundle(RESOURCES);
+		myLevel = level;
 		init();
 	}
 	
@@ -63,7 +81,33 @@ public class PopUpAddLevelTimer extends PopUpParent {
 	}
 	
 	private void addActionBox() {
-		
+		myAction = new ComboBoxLevelTriggerAndAction(myResources.getString(ACTION + PROMPT), ACTION_PROMPT, Arrays.asList(myResources.getString(ACTION + OPTIONS).split(DELIMITER)));
+		myAction.addObserver(this);
+		getContainer().getChildren().add(myAction.createNode());
 	}
 
+	@Override
+	public void update(Observable o, Object arg) {
+		displayActionParameters((String) arg);
+		createLevelTimer();
+	}
+
+	private void displayActionParameters(String name) {
+		myActionCreator = null;
+		switch (name) {
+		case "WinGame":
+			myActionCreator = new WinGameActionCreator(myLevel);
+			break;
+		case "LoseGame":
+			myActionCreator = new LoseGameActionCreator(myLevel);
+			break;
+		case "NextLevel":
+			myActionCreator = new NextLevelActionCreator(myLevel);
+			break;
+		}
+	}
+	
+	private void createLevelTimer() {
+		
+	}
 }
