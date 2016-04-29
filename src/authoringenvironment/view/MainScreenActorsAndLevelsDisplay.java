34eeb017da3aebf431eaaa1a100064a5bd01bfb1
Stephen Kwok <smk44@duke.edu>
2016-04-29 18:27:51 -0400

    Extracting components of Main Screen to new class

---
+++ src/authoringenvironment/view/MainScreenActorsAndLevelsDisplay.java
@@ -0,0 +1,14 @@
+package authoringenvironment.view;
+
+import javafx.scene.layout.*;
+
+public class MainScreenActorsAndLevelsDisplay {
+
+	private HBox myContainer;
+	
+	public MainScreenActorsAndLevelsDisplay() {
+		myContainer = new HBox();
+		
+	}
+	
+}
