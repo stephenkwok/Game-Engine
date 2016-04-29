8ed3ef7ba59335285ba86cfd2d1fd71ec8922373
Blake Kaplan <blakejkaplan@gmail.com>
2016-04-29 18:11:06 -0400

    Fixed Attribute Related Issues

--- src/gameengine/model/Actions/ChangeAttribute.java
+++ src/gameengine/model/Actions/ChangeAttribute.java
@@ -2,7 +2,6 @@ package gameengine.model.Actions;
 
 import gameengine.model.AttributeType;
 import gameengine.model.IGameElement;
-import gameengine.model.IPlayActor;
 
 /**
  * @author blakekaplan
@@ -12,7 +11,7 @@ public class ChangeAttribute extends Action {
 	private AttributeType myType;
 	private int myChange;
 
-    public ChangeAttribute(IGameElement actor1, AttributeType type, int change){
+    public ChangeAttribute(IGameElement actor1, AttributeType type, Integer change){
         super(actor1);
         myType = type;
         myChange = change;
