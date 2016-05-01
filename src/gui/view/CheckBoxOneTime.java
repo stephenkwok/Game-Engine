a6c4a3ef2e229a10284738b0357c5d7f28e8512b
torrescar <cmt57@duke.edu>
2016-05-01 01:01:32 -0400

    testing checkboxonetime

---
+++ src/gui/view/CheckBoxOneTime.java
@@ -0,0 +1,11 @@
+package gui.view;
+
+import authoringenvironment.model.IEditableGameElement;
+
+public class CheckBoxOneTime extends CheckBoxObject {
+
+	public CheckBoxOneTime(String promptText, int width) {
+		super(promptText, width);
+	}
+	
+}
