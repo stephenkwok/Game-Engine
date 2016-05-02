0be598c4e1cd3ee170cdb0b4605fd814986a9e53
torrescar <cmt57@duke.edu>
2016-05-02 00:52:11 -0400

    trying to pull final branch

--- src/authoringenvironment/view/LevelPreview.java
+++ src/authoringenvironment/view/LevelPreview.java
@@ -28,39 +28,24 @@ import javafx.stage.Stage;
 
 
 public class LevelPreview {
-	private static final String PREVIEW_FILE = "preview.xml";
 	private File myPreviewFile;
 	private Controller myController;
-	private GameScreen myView;
-
+	
 	public LevelPreview(Controller controller) {
 		myController = controller;
 	}
 	public void previewGame(){
-		myPreviewFile = new File(PREVIEW_FILE);
+		myPreviewFile = new File("preview.xml");
 		Game model;
 		GameController controller;
-
+		GameScreen view;
+        
 		Group group = new Group();
-		Scene scene = new Scene(group);
-		model = new Game(new GameInfo(), myController.getLevels());
+        Scene scene = new Scene(group);
 
-		addLevelsAndActors(model);
-		saveCurrentGame(model);
-		loadToPlay(model);
-		Stage stage = initCamera(scene, group);
-		controller = initController(model, myView, stage);
-
-		myPreviewFile.delete();
-
-		stage.setOnCloseRequest(e -> {
-			controller.endGame(false);
-		});
-	}
-
-	private void addLevelsAndActors(Game model) {
-		//TODO this is duplicated from controller save game.... also no check for if actors is empty
-		for(Level level: model.getLevels()) {
+        model = new Game(new GameInfo(), myController.getLevels());
+        //TODO this is duplicated from controller save game.... also no check for if actors is empty
+        for(Level level: model.getLevels()) {
 			for (IPlayActor actor: level.getActors()) {
 				if (actor.checkState(ActorState.MAIN)) {
 					level.getMainCharacters().add(actor);
@@ -70,47 +55,45 @@ public class LevelPreview {
 				level.getActors().get(0).addState(ActorState.MAIN);
 			}
 		}
-	}
-	
-	private void saveCurrentGame(Game model) {
-		CreatorController creatorController = new CreatorController(model);
-		try {
+        
+        CreatorController creatorController = new CreatorController(model);
+        try {
 			creatorController.saveForPreviewing(myPreviewFile);
 		} catch (SAXException | IOException | TransformerException | ParserConfigurationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	
-	private void loadToPlay(Game model) {
-		ParserController parserController = new ParserController();
-		model = parserController.loadforPlaying(myPreviewFile);
-	}
-	
-	private Stage initCamera(Scene scene, Group group) {
-		ParallelCamera camera = new ParallelCamera();
-		myView = new GameScreen(camera);
+        ParserController parserController = new ParserController();
+        model = parserController.loadforPlaying(myPreviewFile);
+        
+        ParallelCamera camera = new ParallelCamera();
+        view = new GameScreen(camera);
 
-		SubScene sub = myView.getScene();
-		sub.fillProperty().set(Color.BLUE);
-		group.getChildren().add(sub);
+        controller = new GameController(model, PlayType.PREVIEW);
+        controller.setGame(model);
+        controller.setGameView(view);
 
-		Stage stage = new Stage();
-		stage.setWidth(800);
-		stage.setHeight(600);
+        SubScene sub = view.getScene();
+        sub.fillProperty().set(Color.BLUE);
+        group.getChildren().add(sub);
 
-		sub.setCamera(camera);
-		stage.setScene(scene);
-		stage.show();
-		
-		return stage;
-	}
-	
-	private GameController initController(Game model, GameScreen view, Stage stage) {
-		GameController controller = new GameController(model, PlayType.PREVIEW);
-		controller.setGame(model);
-		controller.setGameView(view);
-		controller.initialize(0);
-		return controller;
+        Stage stage = new Stage();
+        stage.setWidth(800);
+        stage.setHeight(600);
+
+        sub.setCamera(camera);
+        stage.setScene(scene);
+        stage.show();
+        controller.initialize(0);
+        
+        //controller.getView().clearGame();
+        
+        myPreviewFile.delete();
+        
+        stage.setOnCloseRequest(e -> {
+        	controller.endGame(false);
+        });
+        
+        
 	}
 }
