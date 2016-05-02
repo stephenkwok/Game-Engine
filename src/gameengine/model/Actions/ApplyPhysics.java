b8fd5cc7569912d805c1b5616baf85f27fb317bb
Justin Bergkamp <j.bergkamp2@gmail.com>
2016-05-02 13:10:34 -0400

    Commenting the gameengine

--- src/gameengine/model/Actions/ApplyPhysics.java
+++ src/gameengine/model/Actions/ApplyPhysics.java
@@ -2,6 +2,11 @@ package gameengine.model.Actions;
 
 import gameengine.model.Actor;
 
+/**
+ * This action is used to apply forces such as gravity and friction on actors. 
+ * It is most commonly used paired with the TickTrigger
+ * @author justinbergkamp
+ */
 public class ApplyPhysics extends ActorAction {
 
 	public ApplyPhysics(Actor actor) {
--- src/gameengine/model/Actions/GlideForward.java
+++ src/gameengine/model/Actions/GlideForward.java
@@ -2,6 +2,11 @@ package gameengine.model.Actions;
 
 import gameengine.model.Actor;
 
+/**
+ * This action glides an actor forwards(in the direction of the actor's heading) by its given offset. 
+ * 
+ * @author justinbergkamp
+ */
 public class GlideForward extends GlidingAction {
 
 	public GlideForward(Actor actor, Double offset) {
