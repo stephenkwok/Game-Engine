22496d000ab55a1d11742f43fc864048c97b5e96
azhao96 <azhao96@gmail.com>
2016-05-02 03:54:59 -0400

    more comments

--- src/authoringenvironment/view/VBoxWinGameActionCreator.java
+++ src/authoringenvironment/view/VBoxWinGameActionCreator.java
@@ -5,9 +5,18 @@ import gameengine.model.Actions.Action;
 import gameengine.model.Actions.WinGame;
 import javafx.scene.layout.VBox;
 
+/**
+ * VBoxWinGameActionCreator
+ * @author amyzhao
+ *
+ */
 public class VBoxWinGameActionCreator extends VBox implements ILevelActionCreator {
 	private IGameElement myElement;
 	
+	/**
+	 * Constructor for VBoxWinGameActionCreator.
+	 * @param element: element to apply action to.
+	 */
 	public VBoxWinGameActionCreator(IGameElement element) {
 		myElement = element;
 	}
