e8054f3015786cdd102db850b9c13d87d3944418
Colette Torres <colette.torres@duke.edu>
2016-05-02 00:17:08 -0400

    pulling new next level from master

--- src/gameplayer/controller/GameController.java
+++ src/gameplayer/controller/GameController.java
@@ -143,7 +143,6 @@ public class GameController extends Observable implements Observer, IGameControl
 	public void nextLevel() {
 		if (model.nextLevel()) {
 			view.clearGame();
-			model.nextLevel();
 			model.resetLevelTime();
 			begin();
 		}
